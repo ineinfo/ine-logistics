38dd266c0e6989ec0061cfb769aa32ea1a4a8ff6
ineinfotechservices <nikhil.ineinfotech@gmail.com>
2025-02-05 13:11:57 +0530

    update

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,65 +1,97 @@
-import React from 'react';
-import { Row, Col, Divider, Image } from 'antd';
+"use client";
+
+import React from "react";
+import { Row, Col, Image, Grid } from "antd";
+
+const { useBreakpoint } = Grid;
 
 const ProductsList = () => {
+  const screens = useBreakpoint();
   return (
-    <>
-    <div className='mt-48'>
-      <div className='bg-[#76c9f06b] p-10'>
-      <Row justify="space-around">
-        <Col span={4}className='text-center'>
-          <Image
-            width={200}
-            height={200}
-            src="/imgs/home-groundnut1.png"
-            className="mb-2.5"
-          />
-          <h3 className='text-center text-blue-950 text-2xl'>Groundnut</h3>
-        </Col>
-        <Col span={4}>
-        <div className="text-center mt-16 text-blue-950">
-          <h2 className="text-2xl">Procured from the finest producers…</h2>
-          <h3 className="text-xl">packed and dispatched with perfection</h3>
-        </div>
-        </Col>
-        <Col span={4} className='text-center'>
-        <Image
-            width={200}
-            height={200}
-            src="/imgs/p1.png"
-            className="mb-2.5"
-          />
-        <h3 className='text-center text-blue-950 text-2xl'>Turmeric</h3></Col>
-      </Row>
+    <div className="mt-48">
+      <div className="bg-[#76c9f06b] p-10">
+        <Row justify="space-around" gutter={[16, 16]}>
+          <div className="hidden sm:block">
+            <Col span={3} className="text-center  ">
+              <Image
+                width={220}
+                height={220}
+                src="/imgs/home-groundnut1.png"
+                className="mb-2.5 hover:scale-110 transition-transform"
+                preview={true}
+              />
+              <h3 className="text-center text-blue-950 text-2xl font-semibold">
+                Groundnut
+              </h3>
+            </Col>
+          </div>
+          <Col span={24} sm={6} className="text-center">
+            <div className="flex flex-col justify-center items-center min-h-[100%]">
+              <h2 className="text-xl sm:text-2xl font-bold text-blue-950">
+                Procured from the finest producers…
+              </h2>
+              <h3 className="text-lg sm:text-xl text-blue-800">
+                Packed and dispatched with perfection
+              </h3>
+            </div>
+          </Col>
+          <div className=" hidden sm:block">
+            <Col span={3} className="text-center">
+              <Image
+                width={220}
+                height={220}
+                src="/imgs/p1.png"
+                className="mb-2.5 hover:scale-110 transition-transform"
+                preview={true}
+              />
+              <h3 className="text-center text-blue-950 text-2xl font-semibold">
+                Turmeric
+              </h3>
+            </Col>
+          </div>
+        </Row>
 
-      <Row justify="space-evenly">
-        <Col span={4} className='text-center'> 
-        <Image
-            width={200}
-            height={200}
-            src="/imgs/p1.png"
-            className="mb-2.5"
-          />  <h3 className='text-center text-blue-950 text-2xl'>cumin</h3></Col>
-        <Col span={4} className='text-center'> 
-         <Image
-            width={200}
-            height={200}
-            src="/imgs/home-groundnut1.png"
-            className="mb-2.5"
-          />
-          <h3 className='text-center text-blue-950 text-2xl'>Chillies</h3></Col>
-        <Col span={4} className='text-center'> 
-         <Image
-            width={200}
-            height={200}
-            src="/imgs/p1.png"
-            className="mb-2.5"
-          />
-          <h3 className='text-center text-blue-950 text-2xl'>Rice</h3></Col>
-      </Row>
+        {/* Mobile View Adjustment */}
+        <Row gutter={[16, 16]} className="mt-8 sm:justify-evenly">
+          <Col span={8} sm={4} className="text-center">
+            <Image
+              width={screens.sm ? 220 : 100}
+              height={screens.sm ? 220 : 100}
+              src="/imgs/p1.png"
+              className="mb-2.5 hover:scale-110 transition-transform w-full"
+              preview={true}
+            />
+            <h3 className="text-center text-blue-950 text-2xl font-semibold">
+              Cumin
+            </h3>
+          </Col>
+          <Col span={8} sm={4} className="text-center">
+            <Image
+              width={screens.sm ? 220 : 100}
+              height={screens.sm ? 220 : 100}
+              src="/imgs/home-groundnut1.png"
+              className="mb-2.5 hover:scale-110 transition-transform w-full"
+              preview={true}
+            />
+            <h3 className="text-center text-blue-950 text-xl sm:text-2xl font-semibold">
+              Chillies
+            </h3>
+          </Col>
+          <Col span={8} sm={4} className="text-center">
+            <Image
+              width={screens.sm ? 220 : 100}
+              height={screens.sm ? 220 : 100}
+              src="/imgs/p1.png"
+              className="mb-2.5 hover:scale-110 transition-transform w-full"
+              preview={true}
+            />
+            <h3 className="text-center text-blue-950 text-xl sm:text-2xl font-semibold">
+              Rice
+            </h3>
+          </Col>
+        </Row>
+      </div>
     </div>
-    </div>
-    </>
   );
 };
 
