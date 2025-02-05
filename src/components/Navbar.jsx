bfc32dccf735eb2af75b39fc4d4b9c9d94d9ea71
ineinfotechservices <nikhil.ineinfotech@gmail.com>
2025-02-05 17:48:55 +0530

    navbar updated 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,28 +33,35 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div
-      className={`navbar max-w-auto bg-blue-200 text-gray-800 z-50 shadow-lg transition-all duration-500 ease-in-out p-0 m-0 ${
-        isFixed ? "fixed top-0 left-0" : ""
-      }`}
-    >
-      <Flex align="center">
-        <div className="w-[30%] sm:w-[50%] ">
-          <Image src={"/imgs/inelogo.png"} alt="logo" width={120} height={80} />
-        </div>
-        <div className="flex justify-around text-sm sm:text-lg  font-bold w-[70%] sm:w-[50%]">
-          <div className={`cursor-pointer ${getActiveClass("/")}`}>
-            <Link href="/">Home</Link>
+    <>
+      <div
+        className={`navbar max-w-[100%] bg-blue-200 text-gray-800 z-50 shadow-lg transition-all duration-500 ease-in-out p-0 m-0 ${
+          isFixed ? "fixed top-0 left-0" : ""
+        }`}
+      >
+        <Flex align="center">
+          <div className="w-[30%] sm:w-[50%] ">
+            <Image
+              src={"/imgs/inelogo.png"}
+              alt="logo"
+              width={120}
+              height={80}
+            />
           </div>
-          <div className={`cursor-pointer ${getActiveClass("/aboutus")}`}>
-            <Link href="/aboutus">About Us</Link>
+          <div className="flex justify-around text-sm sm:text-lg  font-bold w-[70%] sm:w-[50%]">
+            <div className={`cursor-pointer ${getActiveClass("/")}`}>
+              <Link href="/">Home</Link>
+            </div>
+            <div className={`cursor-pointer ${getActiveClass("/aboutus")}`}>
+              <Link href="/aboutus">About Us</Link>
+            </div>
+            <div className={`cursor-pointer ${getActiveClass("/contactus")}`}>
+              <Link href="/contactus">Contact Us</Link>
+            </div>
           </div>
-          <div className={`cursor-pointer ${getActiveClass("/contactus")}`}>
-            <Link href="/contactus">Contact Us</Link>
-          </div>
-        </div>
-      </Flex>
-    </div>
+        </Flex>
+      </div>
+    </>
   );
 };
 
